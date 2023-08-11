25b28f8cb8c34e43ef8b4459b644af85c94b066d
John Doe <marrian1997@hotmail.com>
2023-08-11 17:48:54 +0100

    mongoDB atlas

--- app.js
+++ app.js
@@ -33,4 +33,5 @@ app.use("/auth", authRoutes);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
+
 module.exports = app;
--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/ghostly-tales-project";
+  "mongodb+srv://mfilipa97:xwuW5DsmejmbpwAX@ghostly.yfyxx4t.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
   .connect(MONGO_URI)
