a9cffd93ab991f3e6ffd1b3ccf4d27027c6b00ae
John Doe <marrian1997@hotmail.com>
2023-08-16 22:39:23 +0100

    CSS and Bootstrap update

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("ghostly-tales-project JS imported successfully!");
 });
+
+$(document).ready(function() {
+  $(".menu-icon").on("click", function() {
+        $("nav ul").toggleClass("showing");
+  });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+  if($(window).scrollTop()) {
+        $('nav').addClass('black');
+  }
+
+  else {
+        $('nav').removeClass('black');
+  }
+})
