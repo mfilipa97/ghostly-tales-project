5d5f40f7f54e79cf059e45d6272d8319d2920f84
John Doe <marrian1997@hotmail.com>
2023-08-17 14:36:52 +0100

    Part of the landing page done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const cookieParser = require('cookie-parser');
+
 
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
@@ -8,9 +10,23 @@ const isLoggedIn = require("../middleware/isLoggedIn");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
+  const visitedBefore = req.cookies.visitedBefore;
   console.log("current user is: ",req.session.currentUser);
   let {currentUser} = req.session;
-  res.render("index",{currentUser});
+
+  if (visitedBefore) {
+    // User has visited before, render regular index page
+    res.render("index",{currentUser});
+
+  }else {
+    // First-time visitor, render the landing page
+    res.render('landing-page',{currentUser},{ layout: false });
+    // Set a cookie to track that the user has visited
+    res.cookie('visitedBefore', 'true', { maxAge: 604800000 }); //7 days to expire
+
+  }
 });
 
+
 module.exports = router;
+
